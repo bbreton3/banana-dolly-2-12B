1c55cf34e957977d72b9fb42c5d34488337e11ad
Benjamin Breton <benjamin.breton@ooshot.com>
2023-04-13 19:13:25 +0200

    fixed import issue

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Potassium("my_app")
 def init():
     device = 0 if torch.cuda.is_available() else -1
     MODEL_NAME = "databricks/dolly-v2-12b"
-    
+
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, padding_side="left")
     model = AutoModelForCausalLM.from_pretrained(
         MODEL_NAME,
@@ -30,7 +30,9 @@ def init():
 # @app.handler is an http post handler running for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
-    
+    model = context.get("model")
+    tokenizer = context.get("tokenizer")
+
     # Start timer
     t_1 = time.time()
 
@@ -43,12 +45,12 @@ def handler(context: dict, request: Request) -> Response:
 
     # Create pipeline with the following parameters
     pipeline = InstructionTextGenerationPipeline(
-        model=model, 
+        model=model,
         tokenizer=tokenizer,
-        do_sample=do_sample, 
-        max_new_tokens=int(max_new_tokens), 
-        top_p=float(top_p), 
-        top_k=int(top_k)
+        do_sample=do_sample,
+        max_new_tokens=int(max_new_tokens),
+        top_p=float(top_p),
+        top_k=int(top_k),
     )
     t_2 = time.time()
 
@@ -57,17 +59,20 @@ def handler(context: dict, request: Request) -> Response:
 
     t_3 = time.time()
 
-    return Response(json={
-        "output": result,
-        "prompt": prompt,
-        "do_sample": do_sample,
-        "max_new_tokens": max_new_tokens,
-        "top_p": top_p,
-        "top_k": top_k,
-        "load_time": t_2 - t_1,
-        "generation_time": t_3 - t_2,
-        "inference_time": t_3 - t_1
-    }, status=200)
+    return Response(
+        json={
+            "output": result,
+            "prompt": prompt,
+            "do_sample": do_sample,
+            "max_new_tokens": max_new_tokens,
+            "top_p": top_p,
+            "top_k": top_k,
+            "load_time": t_2 - t_1,
+            "generation_time": t_3 - t_2,
+            "inference_time": t_3 - t_1,
+        },
+        status=200,
+    )
 
 
 if __name__ == "__main__":
